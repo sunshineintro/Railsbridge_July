d1b5e10ddd7d157ec3cc58380680dfa6d917ba7b
sunshineintro <user@gmail.com>
2012-07-21 15:32:51 -0700

    Made stuff look prettier

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Suggestotron::Application.routes.draw do
   resources :votes
 
   resources :topics
+  
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -59,4 +61,6 @@ Suggestotron::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  
+    root :to => 'topics#index'
 end
